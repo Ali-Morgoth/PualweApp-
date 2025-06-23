7d5489b5e7284051a0c9dca03636dc4ca5a85af2
Ali-Morgoth <alian.andahur.lml@gmail.com>
2025-06-22 21:22:35 -0400

    PWA implementación, manifest.json, icons, layout update, next.config update

--- next.config.mjs
+++ next.config.mjs
@@ -1,4 +1,16 @@
-/** @type {import('next').NextConfig} */
-const nextConfig = {};
+import nextPWA from 'next-pwa'
 
-export default nextConfig;
+/** @type {import('next').NextConfig} */
+const nextConfig = {
+  reactStrictMode: true,
+  // Aquí puedes añadir más opciones de Next.js 
+}
+
+const withPWA = nextPWA({
+  dest: 'public',
+  register: true,
+  skipWaiting: true,
+  disable: process.env.NODE_ENV === 'development',
+})
+
+export default withPWA(nextConfig)
